f14ceec837b5ba5df16777846884878b9106719e
Urbbiz <urbonas_andrius@yahoo.com>
2021-07-26 18:44:56 +0300

    Paterntest added

--- visma-task/tests/Syllable/PatternModel/PatternTest.php
+++ visma-task/tests/Syllable/PatternModel/PatternTest.php
@@ -2,15 +2,22 @@
 
 
 use PHPUnit\Framework\TestCase;
+use Syllable\PatternModel\Pattern;
+use Syllable\PatternModel\PatternCollection;
 use Syllable\PatternModel\PatternExtractor;
 
 class PatternTest extends TestCase
 {
-
     function testGetPatternsWithoutNumber()
-{
-    $patternExtractor = new PatternExtractor("returnPattern");
-    $this->assertInstanceOf(PatternExtractor::class, $patternExtractor);
+    {
+        $pattern = new Pattern("an3dri2us");
+        $this->assertEquals("andrius", $pattern->getPatternWithoutNumbers());
+    }
 
-}
+    function testCreatePattern()
+    {
+        $pattern = new Pattern("returnPattern");
+        $this->assertInstanceOf(Pattern::class, $pattern);
+    }
+
 }
